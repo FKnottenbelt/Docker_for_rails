fa27d39e3206e7b54b559432445be4f50221247d
Femke Knottenbelt <femknotgithub@gmail.com>
2019-04-09 19:45:01 +0200

    Running systemtest with both Selenium and Rack-test

--- myapp/spec/rails_helper.rb
+++ myapp/spec/rails_helper.rb
@@ -2,10 +2,12 @@
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
+
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
+require_relative './support/capybara.rb'
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
@@ -41,6 +43,13 @@ RSpec.configure do |config|
 
   config.before(:each, type: :system) do
     driven_by :rack_test
+  end
+
+  config.before(:each, type: :system, js: true) do
+    driven_by :selenium_chrome_in_container
+    Capybara.server_host = '0.0.0.0'
+    Capybara.server_port = 4000
+    Capybara.app_host = 'http://web:4000'
   end
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
